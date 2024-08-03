053ca1e7b73fbaef5e975dcc96d4d80c904669f3
mzdevI <muhammed.mammadhuseynov@gmail.com>
2024-08-03 21:34:04 +0400

    Add logout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,37 +1,60 @@
 import { Separator } from "@/components/ui/separator.tsx";
 import { Link, useLocation } from "react-router-dom";
-import { Home, Package } from "lucide-react";
+import { Home, LogOut, Package } from "lucide-react";
 import Logo from "@/assets/logo.png";
+import { axiosPrivate } from "@/services/api-client.ts";
+import AuthContext from "@/context/AuthContext.tsx";
+import { useContext } from "react";
 
 const Sidebar = () => {
   const location = useLocation();
+  const { logout } = useContext(AuthContext);
+
+  const handleLogout = async () => {
+    await axiosPrivate.post("/auth/logout");
+    logout();
+  };
 
   return (
-    <nav className="grid bg-white gap-5 items-center text-sm font-medium m-4">
-      <div className="items-center flex justify-center">
-        <img src={Logo} alt="logo" className="h-10 w-10 pointer-events-none" />
+    <nav className="bg-white text-sm font-medium m-4 flex flex-col h-[calc(100vh-2rem)] justify-between">
+      <div className="grid gap-5">
+        <div className="items-center flex justify-center">
+          <img
+            src={Logo}
+            alt="logo"
+            className="h-10 w-10 pointer-events-none"
+          />
+        </div>
+        <Separator />
+        <Link
+          to="/dashboard"
+          className={`flex justify-center items-center p-3 rounded-lg transition-colors hover:text-primary ${
+            location.pathname === "/dashboard"
+              ? "bg-brand-secondary text-white text-primary hover:text-white"
+              : "text-muted-foreground"
+          }`}
+        >
+          <Home className="h-7 w-6" />
+        </Link>
+        <Link
+          to="products"
+          className={`flex justify-center items-center p-3 rounded-lg transition-colors hover:text-primary ${
+            location.pathname === "/dashboard/products"
+              ? "bg-brand-secondary text-white text-primary hover:text-white"
+              : "text-muted-foreground"
+          }`}
+        >
+          <Package className="h-6 w-6" />
+        </Link>
       </div>
-      <Separator />
-      <Link
-        to="/dashboard"
-        className={`flex justify-center items-center p-3 rounded-lg transition-colors hover:text-primary ${
-          location.pathname === "/dashboard"
-            ? "bg-brand-secondary text-white text-primary hover:text-white"
-            : "text-muted-foreground"
-        }`}
-      >
-        <Home className="h-7 w-6" />
-      </Link>
-      <Link
-        to="products"
-        className={`flex justify-center items-center p-3 rounded-lg transition-colors hover:text-primary ${
-          location.pathname === "/dashboard/products"
-            ? "bg-brand-secondary text-white text-primary hover:text-white"
-            : "text-muted-foreground"
-        }`}
-      >
-        <Package className="h-6 w-6" />
-      </Link>
+      <div className="mt-auto">
+        <button
+          onClick={handleLogout}
+          className="p-3 rounded-lg transition-colors hover:text-primary hover:bg-brand-secondary text-muted-foreground hover:text-white"
+        >
+          <LogOut className="h-6 w-6" />
+        </button>
+      </div>
     </nav>
   );
 };
